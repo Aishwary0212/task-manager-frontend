5aadfcccc12c1d39696db5a88a6b1565d3bb85b4
Aishwary <aishwary0212@gmail.com>
2025-11-22 19:50:02 +0530

    backend link upadted

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL="http://localhost:8000";
+export const BASE_URL = process.env.VITE_BACKEND_URL;
 export const API_PATHS = {
     AUTH: {
         REGISTER:"/api/auth/register",
